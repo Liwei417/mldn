cdf2ea8392df2dcfce335ce715c2370bd601e16a
dyh <dyh858@163.com>
2015-07-31 09:35:54 +0800

    Change two files print content

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(String args[]){
 		
-		System.out.println("ÄãºÃ£¬ÊÀ½ç£¡");
+		System.out.println("I'm not with you.Come again?");
 }
--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class Demo{
 	public static void main(String args[]){
 		
-		System.out.println("ÊÀ½çÄãºÃ!");
+		System.out.println("Sorry,I don't follow");
 }
