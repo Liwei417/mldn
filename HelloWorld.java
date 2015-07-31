a5bcc2f6fefe019f02b9431e5e8867cac30e30d0
dyh <dyh858@163.com>
2015-07-31 14:30:35 +0800

    Add a sentence

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(String args[]){
-		
+		System.out.println("I'm not familiar the word.Tell me one more time");
 		System.out.println("I'm not with you.Come again?");
 }
