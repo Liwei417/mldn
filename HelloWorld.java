1eb4822a7292243afea469c1598226fd48a6436d
dyh <dyh858@163.com>
2015-08-01 09:53:34 +0800

    simple print

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,6 @@
-public class Hello{
-	public static String COMPANY_NAME="Eternal"
-	public static void main(String args[]){
-		System.out.println("I'm not familiar the word.Tell me one more time");
-		System.out.println("I'm not with you.Come again?");
-		print();
-	}
-	public static void print() {
-		System.out.println("********************!") ;
-		System.out.println("* Hello World! *") ;
+public class Hell {
+	public static void main(String args[]) {
+		System.out.println("****************");
+		System.out.println("################");
 	}
 }
