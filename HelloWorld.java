91c07af022b38e026c9311f041faac84b47e07f8
dyh858 <dyh858@163.com>
2015-10-27 16:08:12 +0800

    Only Hello World .

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,5 @@
 public class Hell {
-	public static void main(String args[]) {
-		System.out.println("wwww.dyh858.com");
-		System.out.println("www.mldnjava.com");
-		System.out.println("bbs.mldn.cn");
+	public static void main(String args[]) {	
 		System.out.println("Hello World");
-		System.out.println("Hello Enternal");
-		System.out.println("Hppay");
 	}
 }
